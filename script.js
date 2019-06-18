41d272a08080b7089e2a6241deb528d62d1414d4
“[tine <[tinevancorenland@gmail.com]>
2019-06-18 12:07:46 +0200

    make the game work YAS!

--- script.js
+++ script.js
@@ -1,45 +1,68 @@
-console.log("hello");
-
+// Keuze gebruiker
 var rock = document.getElementById("rock");
 var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors");
-var play = document.getElementById("play");
-var winner = document.getElementById("winner");
 
-//Keuze computer in variabele
-
+// Keuze computer
 var choices = ["rock", "paper", "scissors"];
 var randomNumber = Math.floor(Math.random() * choices.length);
 var computerChoice = choices[randomNumber];
 
-function getComputerChoice() {
-  return computerChoice;
+// Speelknop
+var play = document.getElementById("play");
+// Winnaarsboodschap
+var winner = document.getElementById("winner");
+
+// Gebruiker kiest steen door klik
+
+rock.addEventListener("click", playRock);
+
+function playRock() {
+  var userChoice = "rock";
+  console.log(userChoice);
+  console.log(computerChoice);
+  if (computerChoice === "rock") {
+    winner.innerHTML = "Both of you are solid as a rock, no winners here.";
+  } else if (computerChoice === "paper") {
+    winner.innerHTML = "Computer's paper covers your rock, you lo-ho-se-her.";
+  } else {
+    winner.innerHTML =
+      "You broke the computer's scissors. </br> Taste the back of your hand. That's what winner tastes like.";
+  }
 }
 
-//Keuze gebruiker in variabele
+// Gebruiker kiest papier door klik
 
-rock.addEventListener("click", getUserChoice() {
-  var userChoice = "rock";
-  return userChoice;
-  });
+paper.addEventListener("click", playPaper);
 
-
-paper.addEventListener("click", function() {
+function playPaper() {
   var userChoice = "paper";
-});
-scissors.addEventListener("click", function() {
-  var userChoice = "scissors";
-});
-
-
-
-//Variabele vergelijken met computerkeuze wanneer je op play klikt
-//Resultaat geven in "winner" div
-
-play.addEventListener("click", chooseWinner() {
-  if (userChoise === computerChoice) {
-    winner.innerHTML="It's a tie!";
+  console.log(userChoice);
+  console.log(computerChoice);
+  if (computerChoice === "rock") {
+    winner.innerHTML = "Your paper kicks the computer's rock ass, you winner!";
+  } else if (computerChoice === "paper") {
+    winner.innerHTML = "Two papers, two losers.";
+  } else {
+    winner.innerHTML =
+      "You just got cut in half by the computer's scissors. You little wus.";
   }
-});
+}
 
+// Gebruiker kiest schaar door klik
 
+scissors.addEventListener("click", playScissors);
+
+function playScissors() {
+  var userChoice = "scissors";
+  console.log(userChoice);
+  console.log(computerChoice);
+  if (computerChoice === "rock") {
+    winner.innerHTML = "Computer's rock solid, you just got crushed my friend.";
+  } else if (computerChoice === "paper") {
+    winner.innerHTML = "You cut up the computer's paper, you gangsta.";
+  } else {
+    winner.innerHTML =
+      "Scissor scissor on the wall, who is the biggest loser of them all? You both are because you both picked scissors.";
+  }
+}
